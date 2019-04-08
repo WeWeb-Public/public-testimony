f709e606e481c94e3b5dd2e8c3f81e973489c3da
Kramerbaf <nds.kram@gmail.com>
2019-04-08 20:32:16 +0200

    Default Data

---
+++ src/default_data/index.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        name: 'slider',
+        category: 'features',
+        storeName: {
+            en: "Use cases",
+            fr: "Cas d'usage"
+        },
+        storeDesc: {
+            en: "Navigation bar",
+            fr: "Barre de Navigation"
+        }
+    }
+]
---
+++ src/section.vue
@@ -0,0 +1,315 @@
+<template>
+    <div class="jllUseCases">
+        <!-- wwManager:start -->
+        <wwSectionEditMenu v-bind:sectionCtrl="sectionCtrl"></wwSectionEditMenu>
+        <!-- wwManager:end -->
+        <!-- BLOCK MANAGER -->
+        <div class="block-manager">
+            <!-- wwManager:start -->
+            <span v-show="editMode" v-if="section.data.leftBlocks.length > 1"  class="remove-block-btn" @click="deleteBlock()">
+                Remove Block
+            </span>
+            <span v-show="editMode" class="add-block-btn" @click="addBlock()">
+                Add Block
+            </span>
+            <!-- wwManager:end -->
+            <span v-show="section.data.leftBlocks.length > 1">{{this.indexBlock+1}} sur {{this.section.data.leftBlocks.length}}</span>
+            <span v-show="section.data.leftBlocks.length > 1" class="next-block-btn" @click="nextBlock()">
+                <wwObject v-bind:ww-object="section.data.nextBlock"></wwObject>
+            </span>
+        </div>
+        <div class="news-container">
+            <div class="left-container">
+                <div v-for="(block, index) in section.data.leftBlocks" :key="block.uniqueId">
+                    <div class="block" :class="getPos(section.data.leftBlocks, index)">
+                        <!-- BACKGROUND -->
+                        <wwObject class="block-image" v-bind:ww-object="block.bg" ww-default-object-type="ww-image" style="height: 100%" ww-category="background"></wwObject>
+                        <!-- WWOBJS -->
+                        <div class="block-blocks">
+                            <wwLayoutColumn tag="div" ww-default="ww-columns" :ww-list="block.blocks" @ww-add="add(block.blocks, $event)" @ww-remove="remove(block.blocks, $event)">
+                                <div v-for="wwObj in block.blocks" :key="wwObj.uniqueId">
+                                    <wwObject v-bind:ww-object="wwObj"></wwObject>
+                                </div>
+                            </wwLayoutColumn>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right-container">
+                <div v-for="(block, index) in section.data.rightBlocks" :key="block.uniqueId">
+                    <div class="block" :class="getPos(section.data.rightBlocks, index)">
+                        <!-- BACKGROUND -->
+                        <wwObject class="block-image" v-bind:ww-object="block.bg" ww-default-object-type="ww-image" style="height: 100%" ww-category="background"></wwObject>
+                        <!-- WWOBJS -->
+                        <wwLayoutColumn class="block-blocks" tag="div" ww-default="ww-columns" :ww-list="block.blocks" @ww-add="add(block.blocks, $event)" @ww-remove="remove(block.blocks, $event)">
+                            <div v-for="wwObj in block.blocks" :key="wwObj.uniqueId">
+                                <wwObject v-bind:ww-object="wwObj"></wwObject>
+                            </div>
+                        </wwLayoutColumn>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "__COMPONENT_NAME__",
+    props: {
+        sectionCtrl: Object
+    },
+    data() {
+        return {
+            indexBlock: 0
+        }
+    },
+    computed: {
+        section() {
+            return this.sectionCtrl.get();
+        },
+        // wwManager:start
+        editMode() {
+            return this.sectionCtrl.getEditMode() == 'CONTENT'
+        }
+        // wwManager:end
+    },
+    created() {
+        //Initialize section data
+        this.section.data = this.section.data || {};
+        this.section.data.leftBlocks = this.section.data.leftBlocks || [this.getNewLeftBlock()];
+        this.section.data.rightBlocks = this.section.data.rightBlocks || [this.getNewRightBlock()];
+
+        if (!this.section.data.nextBlock) {
+            this.section.data.nextBlock = wwLib.wwObject.getDefault({ type: 'ww-icon', data: { icon: 'wwi wwi-plus' } })
+        }
+
+        this.sectionCtrl.update(this.section)
+    },
+    methods: {
+        // wwManager:start
+        add(array, options) {
+            array.splice(options.index, 0, options.wwObject);
+            this.sectionCtrl.update(this.section);
+        },
+        remove(array, options) {
+            array.splice(options.index, 1);
+            this.sectionCtrl.update(this.section);
+        },
+        
+        addBlock () {
+            this.section.data.leftBlocks.push(this.getNewLeftBlock())
+            this.section.data.rightBlocks.push(this.getNewRightBlock())
+            this.indexBlock = this.section.data.leftBlocks.length - 1
+            this.sectionCtrl.update(this.section)
+        },
+        deleteBlock () { 
+            this.section.data.leftBlocks.splice(this.indexBlock, 1)
+            this.section.data.rightBlocks.splice(this.indexBlock, 1)
+            this.indexBlock = 0
+            this.sectionCtrl.update(this.section)
+        },
+        // wwManager:end
+        getNewRightBlock () {
+            return {
+                uniqueId: wwLib.wwUtils.getUniqueId(),
+                bg: wwLib.wwObject.getDefault({ type: 'ww-color' }),
+                blocks: []
+            }
+        },
+        getNewLeftBlock () {
+            return {
+                uniqueId: wwLib.wwUtils.getUniqueId(),
+                bg: wwLib.wwObject.getDefault({ type: 'ww-image' }),
+                blocks: []
+            }
+        },
+        nextBlock () {
+            this.indexBlock = (this.indexBlock + 1) % this.section.data.leftBlocks.length
+        },
+        getPos (array, idx) {
+            if (this.indexBlock === idx)
+               return 'active'
+            if (idx + 1 === this.indexBlock || (idx === (array.length-1) && this.indexBlock === 0))
+                return 'previous'
+            // if (this.indexBlock + 1 === idx || (this.indexBlock === (array.length-1) && idx === 0))
+            //     return 'next'
+            return ''
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.jllUseCases {
+    position: relative;
+    overflow: hidden;
+
+    .block-manager {
+        position: absolute;
+        bottom: 1%;
+        right: 1%;
+        display: flex;
+        align-items: center;
+        pointer-events: all;
+        z-index: 20000;
+
+        .next-block-btn {
+            cursor: pointer;
+            position: relative;
+            display: inline-block;
+            margin: 0 5px;
+        }
+        .add-block-btn {
+            position: relative;
+            cursor: pointer;
+            background: #19947C;
+            color: white;
+            font-weight: bold;
+            border-radius: 20px;
+            padding: 4px 15px;
+            margin: 0 5px;
+        }
+        .remove-block-btn {
+            position: relative;
+            cursor: pointer;
+            background: #CE003B;
+            color: white;
+            font-weight: bold;
+            border-radius: 20px;
+            padding: 4px 15px;
+            margin: 0 5px;
+        }
+    }
+
+    .news-container {
+        position: relative;
+        display: flex;
+        flex-wrap: wrap;
+        height: 100vh;
+        
+        .block {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: none;
+            background: white;
+            pointer-events: all;
+            overflow-y: auto;
+
+            .block-image {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
+            .block-blocks {
+            }
+            &.active {
+                display: unset;
+                z-index: 2;
+            }
+            &.previous {
+                display: unset;
+                z-index: 1;
+            }
+            &.next {
+            }
+        }
+
+        .left-container {
+            position: relative;
+            width: 50%;
+
+            .block.active {
+                -webkit-animation: 1s block-anim-enter ease-in;
+                -moz-animation: 1s block-anim-enter ease-in;
+                -o-animation: 1s block-anim-enter ease-in;
+                animation: 1s block-anim-enter ease-in;
+                animation-fill-mode: forwards;
+            }
+            .block.previous {
+                -webkit-animation: 1s block-anim-leave ease-in;
+                -moz-animation: 1s block-anim-leave ease-in;
+                -o-animation: 1s block-anim-leave ease-in;
+                animation: 1s block-anim-leave ease-in;
+                animation-fill-mode: forwards;
+                animation-delay: 0.1s;
+            }
+        }
+        .right-container {
+            position: relative;
+            width: 50%;
+
+            .block.active {
+                opacity: 0;
+                -webkit-animation: 1s content-fade-enter 1s ease;
+                -moz-animation: 1s content-fade-enter 1s ease;
+                -o-animation: 1s content-fade-enter 1s ease;
+                animation: 1s content-fade-enter 1s ease;
+                animation-fill-mode: forwards;
+            }
+            .block.previous {
+                opacity: 1;
+                -webkit-animation: 1s content-fade-leave ease-out;
+                -moz-animation: 1s content-fade-leave ease-out;
+                -o-animation: 1s content-fade-leave ease-out;
+                animation: 1s content-fade-leave ease-out;
+                animation-fill-mode: forwards;
+            }
+        }
+    }
+}
+
+@keyframes block-anim-leave { 
+    0% {}
+    100% { transform: translateY(-100%); }
+}
+
+@keyframes block-anim-enter { 
+    0% { transform: translateY(100%); }
+    100% {}
+}
+
+@keyframes content-fade-enter { 
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+}
+
+@keyframes content-fade-leave { 
+    0% { opacity: 1; }
+    100% { opacity: 0; }
+}
+
+@media (max-width: 1024px) {
+    .jllUseCases {
+        .block-manager {
+            top: 51%;
+            bottom: unset;
+        }
+
+        .news-container {
+            height: 100vh;
+            .left-container {
+                width: 100%;
+            }
+            .right-container {
+                width: 100%;
+            }
+        }
+    }
+
+    @keyframes block-anim-leave { 
+        0% {}
+        100% { transform: translateX(100%); }
+    }
+
+    @keyframes block-anim-enter { 
+        0% { transform: translateX(-100%); }
+        100% {}
+    }
+}
+</style>
